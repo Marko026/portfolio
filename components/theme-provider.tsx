f2b6f71d8b55b6be9b8de5a4d3b07f676e8830da
Marko026 <Markec026@yahoo.com>
2024-01-18 02:41:48 +0100

    Remove unnecessary code for setting theme in localStorage

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  localStorage.setItem("theme", `${"dark" === "dark" ? "dark" : "light"}`);
-
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
