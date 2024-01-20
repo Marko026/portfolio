3bf7af9e1297ff6cab139fc2deaf42967f090a9b
Marko026 <Markec026@yahoo.com>
2024-01-20 14:09:16 +0100

    Update styles and remove unused file

---
+++ app/work/[slug]/page.tsx
@@ -0,0 +1,190 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Projects = ({ params }: any) => {
+  return (
+    <div>
+      <div>
+        <div className=" max-w-4xl mx-auto text-center mt-28">
+          <h1 className="h1-semibold ">
+            <span className="bg-gradient-to-r from-[#0026FF] to-[#C1A4FF] bg-clip-text text-transparent">
+              {" "}
+              Morrent
+            </span>{" "}
+            - The modern solution for car rentals
+          </h1>
+          <p className="paragraph-regular text-white-800">
+            Dive into a spectrum of projects that reflect my passion, skill, and
+            commitment
+          </p>
+        </div>
+        <Image
+          src="/images/morent.png"
+          alt="hero"
+          width={500}
+          height={500}
+          className="w-full h-full rounded-lg mt-10 mb-20 max-h-[680px]"
+        />
+      </div>
+      <div className="flex flex-col space-y-14">
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Overview</h2>
+          <div className="w-3/4 base-regular text-white-800">
+            Morrent, our cutting-edge car rental application developed using the
+            robust MERN stack, is revolutionizing the car rental industry. With
+            a dynamic homepage showcasing featured vehicles, convenient pickup
+            and drop-off locations, and a comprehensive list of available cars,
+            It ensures a seamless and visually appealing experience for users.
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Problem</h2>
+          <div className="w-3/4 base-regular text-white-800">
+            Morrent, our cutting-edge car rental application developed using the
+            robust MERN stack, is revolutionizing the car rental industry. With
+            a dynamic homepage showcasing featured vehicles, convenient pickup
+            and drop-off locations, and a comprehensive list of available cars,
+            It ensures a seamless and visually appealing experience for users.
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Tech Stack</h2>
+          <div className="w-3/4 base-regular text-white-800">
+            Morrent, our cutting-edge car rental application developed using the
+            robust MERN stack, is revolutionizing the car rental industry. With
+            a dynamic homepage showcasing featured vehicles, convenient pickup
+            and drop-off locations, and a comprehensive list of available cars,
+            It ensures a seamless and visually appealing experience for users.
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Reflection</h2>
+          <div className="w-3/4 base-regular text-white-800">
+            Morrent, our cutting-edge car rental application developed using the
+            robust MERN stack, is revolutionizing the car rental industry. With
+            a dynamic homepage showcasing featured vehicles, convenient pickup
+            and drop-off locations, and a comprehensive list of available cars,
+            It ensures a seamless and visually appealing experience for users.
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div className="w-1/4 ">
+            <h2 className="h2-normal">Final Site</h2>
+            <Link href={""} className="paragraph-regular text-white-800">
+              morrent.vercel.app
+              <Image
+                src="/icons/Vector.svg"
+                width={10}
+                height={10}
+                alt="arrow"
+                className="inline-block ml-2"
+              />
+            </Link>
+          </div>
+          <div className="w-3/4 bg-black-200 bg-opacity-50">
+            <div className="flex justify-center ">
+              <Image
+                src="/images/laptop.png"
+                alt="hero"
+                width={500}
+                height={500}
+                className="w-3/5 rounded-lg mt-10 mb-20 max-h-[532px]"
+              />
+              <Image
+                src="/images/small-phone.png"
+                alt="hero"
+                width={500}
+                height={500}
+                className="w-1/6 rounded-lg  mb-20 max-h-[332px] mt-auto"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Learning</h2>
+          <ol className="w-3/4 base-regular list-decimal space-y-6 text-white-800">
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+          </ol>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Challenges</h2>
+          <ol className="w-3/4 base-regular list-decimal space-y-6 text-white-800">
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+            <li>
+              Gained extensive knowledge of full authentication & authorization
+              implementation, including the use of industry-standard security
+              protocols such as JWT
+            </li>
+          </ol>
+        </div>
+        <div className="flex justify-between">
+          <h2 className="w-1/4 h2-normal">Future</h2>
+          <div className="w-3/4 base-regular text-white-800">
+            Morrent, our cutting-edge car rental application developed using the
+            robust MERN stack, is revolutionizing the car rental industry. With
+            a dynamic homepage showcasing featured vehicles, convenient pickup
+            and drop-off locations, and a comprehensive list of available cars,
+            It ensures a seamless and visually appealing experience for users.
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between my-28">
+        <div className="">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/icons/arrow-left.svg"
+              alt="hero"
+              width={30}
+              height={30}
+            />
+            <p className="h2-normal">Previous Study</p>
+          </div>
+          <p className="text-white-800">JobIT - Job Finding App</p>
+        </div>
+        <div className="">
+          <div className="flex items-center gap-2">
+            <p className="h2-normal">Previous Study</p>
+            <Image
+              src="/icons/arrow-right.svg"
+              alt="hero"
+              width={30}
+              height={30}
+            />
+          </div>
+          <p className="text-white-800">Filmpire - AI Movie App</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- components/ProjectCard/page.tsx
+++ components/ProjectCard/page.tsx
@@ -12,24 +12,26 @@ import { allProjects } from "@/types";
 import { urlFor } from "@/sanity/lib/sanity";
 const ProjectCard = ({ project }: { project: allProjects }) => {
   return (
-    <Link href={`/work/${project.slug}`} key={project._id} className="w-3/5 ">
-      <Card className="border-none">
-        <CardHeader className="pb-2">
-          <Image
-            src={urlFor(project.imageTitle).url()}
-            alt={project.title}
-            width={300}
-            height={200}
-            priority={true}
-            className="object-contain w-full h-full rounded-lg mb-3"
-          />
-          <CardTitle className="">{project.title}</CardTitle>
-        </CardHeader>
-        <CardContent className="pb-0">
-          <p className="base-regular text-white-800">
-            Unleashing Opportunities in Your Job Search Journey
-          </p>
-        </CardContent>
+    <Link href={`/work/${project.slug}`} key={project._id} className="w-3/6 ">
+      <Card className="border-none bg-black-200">
+        <div className="bg-white rounded-xl shadow-md hover:shadow-glow duration-300">
+          <CardHeader className="pb-2">
+            <Image
+              src={urlFor(project.imageTitle).url()}
+              alt={project.title}
+              width={300}
+              height={200}
+              priority={true}
+              className="object-contain w-full h-full rounded-lg mb-3"
+            />
+            <CardTitle className="">{project.title}</CardTitle>
+          </CardHeader>
+          <CardContent className="mb-5">
+            <p className="base-regular text-white-800">
+              Unleashing Opportunities in Your Job Search Journey
+            </p>
+          </CardContent>
+        </div>
       </Card>
     </Link>
   );
