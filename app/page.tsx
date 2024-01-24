c7f05c84a2e27ee3184c662e57c42baa9c9f1850
Marko026 <Markec026@yahoo.com>
2024-01-24 16:38:46 +0100

    Update page layout and add link to work

--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,23 @@ export default async function Home() {
           Web developer creating top-notch digital experiences with passion and
           precision
         </p>
+        <Link
+          href="/work"
+          className="mt-9 px-10 inline-block py-4 gradient rounded-full hover:scale-110 duration-500">
+          <div className="px-4 py-2 md:px-6 md:py-4 rounded-full flex items-center gap-2 bg-gradient-to-r from-[#0026FF] to-[#C1A4FF]">
+            <p className="paragraph-normal ">See my work</p>
+            <Image src="/icons/Vector.svg" width={10} height={10} alt="arrow" />
+          </div>
+        </Link>
+        <Image
+          src="/images/dev.png"
+          width={850}
+          height={567}
+          priority
+          className="object-cover h-full w-full"
+          alt="dev"
+        />
       </div>
-      <Link
-        href="/work"
-        className="mt-9 px-10 py-4 gradient rounded-full hover:scale-110  duration-500">
-        <div className="px-4 py-2 md:px-6 md:py-4 rounded-full flex items-center gap-2 bg-gradient-to-r from-[#0026FF] to-[#C1A4FF]">
-          <p className="paragraph-normal ">See my work</p>
-          <Image src="/icons/Vector.svg" width={10} height={10} alt="arrow" />
-        </div>
-      </Link>
-      <Image
-        src="/images/dev.png"
-        width={850}
-        height={567}
-        priority
-        className="object-cover"
-        alt="dev"
-      />
     </div>
   );
 }
