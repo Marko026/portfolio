a1b5221ec31688ceabcfa970917519a0da611052
Marko026 <Markec026@yahoo.com>
2024-01-24 16:28:09 +0100

    Refactor ProjectCard image display

--- components/ProjectCard/page.tsx
+++ components/ProjectCard/page.tsx
@@ -19,14 +19,17 @@ const ProjectCard = ({ project }: { project: allProjects }) => {
       <Card className="border-none bg-black-200 p-3 shadow-md hover:shadow-glow duration-300">
         <div className="bg-white rounded-xl ">
           <CardHeader className="!p-0">
-            <Image
-              src={urlFor(project.imageTitle).url()}
-              alt={project.title}
-              width={300}
-              height={200}
-              priority={true}
-              className="object-contain w-full h-full rounded-lg md:mb-3"
-            />
+            <div className="relative">
+              <Image
+                src={urlFor(project.imageTitle).url()}
+                alt={project.title}
+                width={300}
+                height={200}
+                priority={true}
+                className="object-contain w-full h-full rounded-lg md:mb-3"
+              />
+              <div className="absolute left-0 right-0 h-1/3 bottom-0 bg-gradient-to-t from-black-200 to bg-transparent"></div>
+            </div>
             <CardTitle className="h2-normal !mb-2">{project.title}</CardTitle>
           </CardHeader>
           <CardContent className="!p-0 ">
