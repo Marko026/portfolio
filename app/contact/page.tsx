a4e79272956a722bfb8dd0bae0bfd4f0442a6f08
Marko026 <Markec026@yahoo.com>
2024-01-25 10:35:24 +0100

    Add key prop to Link component and delete unused schemas

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,6 +13,7 @@ const Contact = () => {
       <div className="flex justify-between gap-3 flex-wrap mx-auto py-20 mb-[200px]">
         {links.map((link) => (
           <Link
+            key={link.href}
             href={link.href}
             target="_blank"
             className="grow sm:basis-2/5 lg:basis-1/5 shadow-md hover:shadow-glow duration-200 hover:scale-105 bg-black-200 bg-opacity-20 backdrop-blur-xl flex items-center justify-center rounded-[8px] py-3 px-7 ">
