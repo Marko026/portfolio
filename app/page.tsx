7bb4d06d6f1f0b1e21945de45ae5fd03116d1a1a
MarkoTodorovic <markec026@yahoo.com>
2024-02-02 12:01:02 +0100

    Remove link to work page

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,7 @@ export default async function Home() {
     <div className="flex flex-col items-center mt-10 md:mt-20 ">
       <div className="max-w-4xl text-center m-0">
         <SpotlightPreview />
-        <Link
-          href="/work"
-          className="sm:mt-9 px-10 inline-block py-4 gradient rounded-full hover:scale-110 duration-500">
-          <div className="px-4 py-2 md:px-6 md:py-4 rounded-full flex items-center gap-2 bg-gradient-to-r from-[#0026FF] to-[#C1A4FF]">
-            <p className="paragraph-regular text-white-900">See my work</p>
-            <Image src="/icons/Vector.svg" width={10} height={10} alt="arrow" />
-          </div>
-        </Link>
+
         <GradientShadowButton />
         <Image
           src="/images/dev.png"
