afe60f290699dab2d19d9af234e10264e765617b
MarkoTodorovic <markec026@yahoo.com>
2024-02-09 14:18:31 +0100

    Update contact page styling and form

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -23,20 +23,20 @@ const Contact = () => {
         </h1>
       </MotionDiv>
       <Form />
-      <div className="flex justify-between gap-3 flex-wrap mx-auto py-20 mb-[200px]">
+      <div className="flex justify-between shimmer gap-3 overflow-hidden flex-wrap mx-auto px-1 py-20 mb-[200px]">
         {links.map((link) => (
           <Link
             key={link.href}
             href={link.href}
             target="_blank"
-            className="grow basis-2/5 lg:basis-1/5 shadow-md hover:shadow-glow duration-500 hover:scale-105 bg-black-200 bg-opacity-70 backdrop-blur-xl flex items-center justify-center rounded-[8px] py-3 px-7 ">
+            className="grow basis-2/5 lg:basis-1/5 duration-500 hover:scale-105 bg-black-200 bg-opacity-70 backdrop-blur-xl flex items-center justify-center rounded-[8px] py-3 px-7 ">
             <Image
               src={link.src}
               width={20}
               height={20}
               alt={link.alt}
               priority={true}
-              className="inline-block ml-2 "
+              className="inline-block ml-2"
             />
             <span className="ml-3 text-white-900">{link.text}</span>
           </Link>
