3917e836c31fdb08cc1855e590c0a75c6f1f270b
Marko026 <Markec026@yahoo.com>
2024-02-12 02:46:36 +0100

    Update link styling in SeeMyWork page

--- components/SeemyWork/page.tsx
+++ components/SeemyWork/page.tsx
@@ -6,7 +6,7 @@ const LinkMyWork = () => {
     <div className="group sm:mt-9 mx-auto relative w-fit transition-transform duration-300 active:scale-95">
       <Link
         href="/work"
-        className=" px-2 md:px-6  rounded-full flex items-center gap-2 relative z-0 overflow-hidden border-gray-700 border 
+        className=" px-2 md:px-4  rounded-2xl flex items-center gap-2 relative z-0 overflow-hidden border-gray-700 border 
         bg-transparent  font-semibold
         uppercase text-violet-300 transition-all duration-500
         before:absolute before:inset-0
@@ -20,7 +20,7 @@ const LinkMyWork = () => {
         active:scale-95">
         <div className="px-4 py-2 md:px-6 flex gap-2 md:py-4 rounded-full">
           <p className="paragraph-regular text-white-900">See my work</p>
-          <Image src="/icons/Vector.svg" width={10} height={10} alt="arrow" />
+          <Image src="/icons/Vector.svg" width={12} height={12} alt="arrow" />
         </div>
       </Link>
     </div>
